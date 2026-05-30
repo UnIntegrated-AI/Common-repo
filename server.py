a158a536c6ee6b30452506f8a28da42cb3a13b6f
Cʀʏᴘᴛɪᴄ <117926278+UnIntegrated-AI@users.noreply.github.com>
2026-05-30 17:48:38 +0530

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ cursor = conn.cursor()
 
 cursor.execute("create database if not exists secure_chat_room")
 cursor.execute("use secure_chat_room")
-cursor.execute("create table if not exists clients(username char(50) primary key, password varchar(50) not null, banned bool default False)")
+cursor.execute("create table if not exists clients(user char(50) primary key, password varchar(50) not null, banned bool default False)")
 
 HOST = "127.0.0.1" # Put Local IP Address to use multiple devices on same network
 PORT = 5000 # putting unique port here
@@ -25,16 +25,17 @@ server.listen() # starts looking for connection
 clients = [] # list of online clients
 usernames = [] # list of online usernames
 ban_list = []
+commands = ['ban','kick']
 
-# def fetch_user():
-#     cursor.execute(f"select username from clients where banned = False")
-#     user = cursor.fetchall()
-#     if user:
-#         return True
-#     else:
-#         return False
+def find_user(username):
+    cursor.execute(f'select * from clients where user = "{username}"')
+    inf = cursor.fetchone()
+    if inf:
+        return True
+    else:
+        return False
 
-cursor.execute("select username from clients where banned = True")
+cursor.execute("select user from clients where banned = True")
 buser = cursor.fetchall()
 for buser in buser:
     ban_list.append(buser)
@@ -49,27 +50,56 @@ def broadcast(msg):
 
 # handle clients
 
-def handle(client):
+def handle(client, uname):
     while True:
         try:        # if connection is open
             message = client.recv(1024).decode() # receving messages from clients
+            if uname == "kirmada":
+                if message[len(uname)+2] == "?": # kirmada: ?kick username
+                    
+                    command = message[len(uname)+3:]
+                    tname = command.split(" ")
+                    cmd = tname[0]
+                    tname = tname[1]
+                    if  cmd in commands:
+                        if  cmd == commands[0]:
+                            ban_user(tname)
+                            break
+                        elif cmd == commands[1]:
+                            # client.send("You are kicked by kirmada")
+                            
+                            kick_user(tname)
+                            break
+
+
             print(f'{message}') # log on server
-            if message == "Admin":
-                client.send("Get Admin User".encode())
-                username = client.recv(1024).decode()
-                broadcast(f"Beware! {username} has joined the server".encode())
-
             broadcast(message.encode()) # sending to client
-
-                        
         except:         # if connection is closed
-            index = clients.index(client) # fetching index of client
             clients.remove(client) # removing client
             client.close() # closing client connection
-            username = usernames[index]
-            broadcast(f"{username} left the chat!".encode())
-            usernames.remove(username) # removing username of client
+            broadcast(f"{uname} left the chat!".encode())
+            usernames.remove(uname) # removing username of client
             break
+
+def ban_user(tname):
+    name_index = usernames.index(tname)
+    client_to_ban = clients[name_index]
+    clients.remove(client_to_ban)
+    client_to_ban.send("You were banned from the Server!")
+    client_to_ban.close()
+    cursor.execute(f'update clients set banned = True where user = "{tname}"')
+    conn.commit()
+    broadcast(f"{tname} has been banned from the chat".encode())
+
+def kick_user(tname):
+    if tname in usernames:
+        name_index = usernames.index(tname)
+        client_to_kick = clients[name_index]
+        clients.remove(client_to_kick)
+        client_to_kick.send("You were kicked by kirmada!".encode())
+        client_to_kick.close()
+        usernames.remove(client_to_kick)
+        broadcast(f"{tname} has been kicked from the chat".encode())
 
 # recieve messages
     
@@ -83,45 +113,41 @@ def recieve():
         client.send("Get Password".encode()) # recieving username
         password = client.recv(1024).decode()
         
-        if username in ban_list: # checking if user is banned or not
-            client.send("you are banned".encode())
-            broadcast(f"Banned user: {username} tried to join the chat but our security took over".encode())
-            client.close() # closing connection
-            break 
+        if not find_user(username):
+            print("register page entered")
+            cursor.execute(f'insert into clients(user, password) values("{username}", "{password}")')
+            conn.commit()
+            print(f"{username} just signned up")
         else:
-            print("aa rha h")
-            cursor.execute(f"select username from clients where banned = False and username = {username}")
-            user = cursor.fetchone()
-            if not user: # if user doesn't exists in databse
-                cursor.execute(f"insert into clients values({username}, {password}, null)") # register new user
-                usernames.append(username) # adding username to server's database
-            else:
-                cursor.execute(f"select password from clients where username = {username}") # retriving password if user exists
-                passwd = cursor.fetchall()[0]
-                if password == passwd: # checking if entered password matches the actual one
-                    pass
-                else:
-                    client.send("You entered wrong password".encode()) 
-                    client.close() # closing connection is password is wrong
+            print("trying fetch pass")
+            cursor.execute(f'select user,password from clients where user = "{username}"')
+            info = cursor.fetchone()
+            
+            if info[0] == "kirmada" and info[1] == "passwd":
+                print('Admin has joined.')
+                broadcast("Admin has joined the chat (Be Aware!)".encode())
+                client.send("Welcome Admin!".encode())
+            elif info[1] == password:
+                if username in ban_list:
+                    client.send("you are banned".encode())
+                    print(f"banned user {username} tried to login")
+                    client.close()
                     break
-        
+                
+                print(f"{username} logged in successfully")
+                client.send("login successful".encode())
+            else:
+                client.send("wrong password".encode())
+                client.close()
 
-        # Add conditions for username to create a admin page
-
-        if username == "Kirmada" and password == "passwd":
-            client.send(f"Welcome {username}".encode())
-            broadcast(f"\nBeware! {username} has joined the server.\n".encode())
-
-
-
-        # clients.append(client) # adding client to server's client list
+        clients.append(client) # adding client to server's client list
 
 
         print(f'{username} joined the chat!\n') # log of client in server
         broadcast(f'{username} joined the chat!'.encode()) # broadcasts the joining of client to all clients
         client.send("Connected To the Server!".encode()) # confirmation for client
 
-        thread = threading.Thread(target=handle, args=(client,)) # create a thread for client for handling clients
+        thread = threading.Thread(target=handle, args=(client,username)) # create a thread for client for handling clients
         thread.start() # start the thread
 
 print("Server running...")
